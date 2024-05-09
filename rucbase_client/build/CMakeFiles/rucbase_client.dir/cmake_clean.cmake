3113798b4659c8a0efc94e3fa848e37fe3c6e131
qwer-ty-uiop <185649066@qq.com>
2024-05-09 16:33:44 +0800

    main:1

---
+++ rucbase_client/build/CMakeFiles/rucbase_client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rucbase_client.dir/main.cpp.o"
+  "rucbase_client"
+  "rucbase_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rucbase_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
