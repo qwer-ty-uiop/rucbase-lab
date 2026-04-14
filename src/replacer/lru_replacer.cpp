07e6614767b86286e9c85e8a67e17c311cce8594
TY <18649066@qq.com>
2026-04-15 00:13:45 +0800

    refactor(replacer): 优化 LRU Replacer 代码和注释
    - 移除已完成的 Todo 注释
    - 完善函数注释，添加参数说明和功能描述
    - 添加代码注释解释关键步骤
    - 使用 empty() 替代 Size() == 0 判断
    - 统一代码风格
    
    测试通过: LRUReplacerTest, BufferPoolManagerTest, BufferPoolManagerConcurrencyTest, StorageTest

--- src/replacer/lru_replacer.cpp
+++ src/replacer/lru_replacer.cpp
@@ -15,52 +15,52 @@ LRUReplacer::LRUReplacer(size_t num_pages) : max_size_(num_pages) {}
 LRUReplacer::~LRUReplacer() = default;
 
 /**
- * @description: 使用LRU策略删除一个victim frame，并返回该frame的id
- * @param {frame_id_t*} frame_id
- * 被移除的frame的id，如果没有frame被移除返回nullptr
+ * @description: 淘汰一个页面，并返回该frame的id
+ * @param {frame_id_t*} frame_id 被移除的frame的id，如果没有frame被移除返回nullptr
  * @return {bool} 如果成功淘汰了一个页面则返回true，否则返回false
  */
 bool LRUReplacer::victim(frame_id_t* frame_id) {
-    // C++17 std::scoped_lock
-    // 它能够避免死锁发生，其构造函数能够自动进行上锁操作，析构函数会对互斥量进行解锁操作，保证线程安全。
-    std::scoped_lock lock{latch_};  //  如果编译报错可以替换成其他lock
-    if (Size() == 0) {
+    std::scoped_lock lock{latch_};
+    
+    if (LRUlist_.empty()) {
         return false;
     }
+    
+    // 选择最近最少使用的页面（list尾部）作为淘汰页面
     *frame_id = LRUlist_.back();
     LRUlist_.pop_back();
     LRUhash_.erase(*frame_id);
+    
     return true;
-    // Todo:
-    //  利用lru_replacer中的LRUlist_,LRUHash_实现LRU策略
-    //  选择合适的frame指定为淘汰页面,赋值给*frame_id
 }
 
 /**
  * @description: 固定指定的frame，即该页面无法被淘汰
- * @param {frame_id_t} 需要固定的frame的id
+ *               从LRU列表中移除该frame，因为它正在被使用
+ * @param {frame_id_t} frame_id 需要固定的frame的id
  */
 void LRUReplacer::pin(frame_id_t frame_id) {
     std::scoped_lock lock{latch_};
-    // Todo:
-    // 固定指定id的frame
-    // 在数据结构中移除该frame
+    
+    // 在hash中查找该frame
     auto it = LRUhash_.find(frame_id);
+    
+    // 如果找到，从LRU列表中移除（正在使用，不能被淘汰）
     if (it != LRUhash_.end()) {
-        LRUlist_.erase(LRUhash_[frame_id]);
+        LRUlist_.erase(it->second);
         LRUhash_.erase(frame_id);
     }
 }
 
 /**
  * @description: 取消固定一个frame，代表该页面可以被淘汰
+ *               将该frame加入LRU列表头部（最近使用，最不可能被淘汰）
  * @param {frame_id_t} frame_id 取消固定的frame的id
  */
 void LRUReplacer::unpin(frame_id_t frame_id) {
-    // Todo:
-    //  支持并发锁
-    //  选择一个frame取消固定
     std::scoped_lock lock{latch_};
+    
+    // 如果该frame不在LRU列表中，才加入
     if (LRUhash_.find(frame_id) == LRUhash_.end()) {
         LRUlist_.push_front(frame_id);
         LRUhash_[frame_id] = LRUlist_.begin();
